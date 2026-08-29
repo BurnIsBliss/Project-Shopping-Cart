2754d3c6844a491d3b975ab830d192a93e5b0d33
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2026-08-30 00:56:03 +0530

    Tried to fix the image not being displayed with vercel deployment, but seems like vercel does not allow long term storage of images.

--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -26,24 +26,24 @@ export default function HomePage() {
 			<a href="shop">Click me!</a>
 			<div>
 				<img
-					src="../src/images/Discount.jpg"
+					src="/src/images/Discount.jpg"
 					alt="50% discount"
 					className={styles.imgStyle}
 				/>
 				<img
-					src="../src/images/openShop.jpg"
+					src="/src/images/openShop.jpg"
 					alt="Open shop"
 					className={styles.imgStyle}
 				/>
 				<img
-					src="../src/images/BuyNow.jpg"
+					src="/src/images/BuyNow.jpg"
 					alt="Buy now"
 					className={styles.imgStyle}
 				/>
 			</div>
 			<div className={styles.div247} id="toggle">
 				<img
-					src="../src/images/open247.jpg"
+					src="/src/images/open247.jpg"
 					alt="Open 24x7"
 					className={styles.imgStyle}
 				/>
